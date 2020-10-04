ab9dc14d5aae5e6a72413513ba215893e6a84f7d
MD FAIZ ANSARI <faizansari123z@gmail.com>
2020-10-04 14:23:37 +0530

    Update: Apollo frontend setup (#2)
    * Update: Apollo setup
    
    * Update: Apollo setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { gql, useMutation } from "@apollo/client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import * as serviceWorker from "./serviceWorker";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:8080/graphql",
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </ApolloProvider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change

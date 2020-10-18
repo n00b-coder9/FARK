b8a8da33347f467217908994479851465eec1798
Rishav Agarwal <rishavcoder@gmail.com>
2020-10-18 18:59:26 +0530

    Update homepage UI (#11)
    * Update homepage ui
    
    * Minor fixes
    
    * fix: home ui

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -5,10 +5,13 @@ import {
   useMediaQuery,
   useTheme,
   Card,
-  Button, CardActions,
+  CardActions,
+  Button,
+  InputBase,
   CardHeader,
-  LinearProgress,
+  Fade,
 } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
 import { validateUrl } from '../../utils/validator';
 import { useDispatch, useSelector } from 'react-redux';
 import { setIsSnackbarOpen } from '../../redux/slices/snackbar';
@@ -21,29 +24,37 @@ const useStyles = makeStyles((theme) => ({
   root: {
     'display': 'flex',
     'flexDirection': 'column',
-    'justifyContent': 'center',
-    'alignItems': 'center',
     'height': '100%',
+    'width': 'min(740px, 100%)',
+    'margin': 'auto',
     '& form': {
       display: 'flex',
-      flexDirection: 'column',
     },
     '& .MuiTextField-root': {
       margin: theme.spacing(1),
     },
   },
-  generateButton: {
-    padding: '16px',
-    width: '250px',
+  textBarSpacing: {
+    margin: theme.spacing(1),
+    marginLeft: theme.spacing(2),
   },
-  cardHeader: {
-    height: '20%',
-    width: '100%',
-    color: 'white',
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign: 'center',
+  primaryBgColor: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.getContrastText(theme.palette.primary.main),
+  },
+  primaryContrastTextColor: {
+    'color': theme.palette.getContrastText(theme.palette.error.main),
+    '&.active': {
+      color: 'inherit',
+    }, '&.focus': {
+      color: 'inherit',
+    },
+    '&.hover': {
+      color: 'inherit',
+    },
+    '&.Mui-disabled': {
+      color: 'gray',
+    },
   },
 }));
 
@@ -64,6 +75,7 @@ function Home() {
 
   const [longUrl, setLongUrl] = useState('');
   const [shortUrl, setShortUrl] = useState(isShortenAndAuth ? shortenDataAfterAuth.shortUrl : '');
+  const [queryLongUrl, setQueryLongUrl] = useState('');
   const [isFormEnabled, setFormEnabled] = useState(true);
   const [longUrlHasError, setLongUrlHasError] = useState(false);
   const [longUrlErrMsg, setLongUrlErrMsg] = useState('');
@@ -84,22 +96,31 @@ function Home() {
     }
     setFormEnabled(false);
     setLongUrlHasError(false);
+    setDetailsFormEnabled(false);
     // check if url is valid
-    const UrlValidity = validateUrl(longUrl);
-    const errorFree = UrlValidity.isValid;
+    const urlValidity = validateUrl(longUrl);
+    const errorFree = urlValidity.isValid;
 
     if (!errorFree) {
       setLongUrlHasError(true);
-      setLongUrlErrMsg(UrlValidity.message);
+      setLongUrlErrMsg(urlValidity.message);
       // if url is invalid ask the user to resubmit the form
+      setDetailsFormEnabled(true);
       return setFormEnabled(true);
     }
+
+    // If we are querying for the new url, disable the older one
+    setIsShortUrlGen(false);
+
     // if error free then try to generate shortUrl
     try {
       const graphqlQuery = shortenUrlQuery({ longUrl, userId: userId === null ? 'guest' : userId });
       const response = await axios.post('/', graphqlQuery);
+
       setShortUrl(response.data.data.shortenUrl.shortUrl);
+      setQueryLongUrl(response.data.data.shortenUrl.longUrl);
       setFormEnabled(true);
+      setDetailsFormEnabled(true);
       return setIsShortUrlGen(true);
     } catch (err) {
       // allow user to resubmit the form
@@ -115,6 +136,7 @@ function Home() {
         }));
       }
       setFormEnabled(true);
+      setDetailsFormEnabled(true);
     }
   };
 
@@ -125,9 +147,9 @@ function Home() {
     }
     setDetailsFormEnabled(false);
     if (isLoggedIn) {
-      /* TODO: if user is logged in then allow him to add title/description */
+      /* TODO: User is logged in, editing url details */
     } else {
-      /* if user is not logged in then redirect him to the auth page*/
+      /* User is not logged in, redirect to the auth page*/
       history.push({
         pathname: '/auth', state: {
           from: location,
@@ -138,165 +160,204 @@ function Home() {
       });
     }
   };
-  // the base url
-  const baseUrl = axios.getConfig().baseURL;
-  // content to be rendered before short url is generated
-  let content = (
-    <div style={{
-      flexDirection: 'column',
-      padding: mediaMinSm ? '24px' : '4px',
-      paddingLeft: mediaMinSm ? '32px' : '6px',
-      paddingRight: mediaMinSm ? '32px' : '6px',
-    }}>
-      <form noValidate
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleLongUrlform();
-        }}>
-        <TextField
-          label="BASE URL"
-          id="base-url"
-          variant="filled"
-          value={baseUrl}
-          InputProps={{
-            readOnly: true,
-          }}
-        />
-        <TextField
-          error={longUrlHasError}
-          helperText={longUrlErrMsg}
-          value={longUrl}
-          label="Long url"
-          placeholder="www.example.com/a/very/long/url?query=a+very+long+query"
-          onChange={(e) => {
-            setLongUrl(e.target.value);
-            setLongUrlHasError(false);
-            setLongUrlErrMsg('');
-          }}/>
-        <CardActions>
-          <Button
-            disabled={!isFormEnabled}
-            type="submit"
-            color="primary"
-            variant="contained"
-            style={{ marginLeft: 'auto' }}>
-        Create
-          </Button>
-        </CardActions>
-      </form>
-    </div>);
 
-  // if shorturl has been generated render this instead
+  // Long url input form
+  let form = (
+    <div style={{
+      marginTop: '10vh',
+    }}>
+      <Card>
+        <form noValidate
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleLongUrlform();
+          }}>
+          {/* Type long url here */}
+          <InputBase
+            autoFocus
+            className={classes.textBarSpacing}
+            style={{ flexGrow: 1 }}
+            value={longUrl}
+            label="Long url"
+            placeholder="www.example.com/a/very/long/url?query=a+very+long+query"
+            onChange={(e) => {
+              setLongUrl(e.target.value);
+              setLongUrlHasError(false);
+              setLongUrlErrMsg('');
+            }} />
+          <CardActions className={classes.primaryBgColor}>
+            {/* Submit shortening request */}
+            <Button
+              className={classes.primaryContrastTextColor}
+              disabled={!isFormEnabled}
+              type="submit"
+              style={{ marginLeft: 'auto' }}>
+              Shorten
+            </Button>
+          </CardActions>
+        </form>
+      </Card>
+      {/* Error message */}
+      <Fade in={longUrlHasError}>
+        <div style={{ color: 'red', height: '1em', margin: '8px' }}>{longUrlErrMsg}</div>
+      </Fade>
+    </div>
+  );
+
+  // If short url has been generated render the final form
   if (isShortUrlGen) {
-    content = (
-      <div style={{
-        flexDirection: 'column',
-        padding: mediaMinSm ? '24px' : '4px',
-        paddingLeft: mediaMinSm ? '32px' : '6px',
-        paddingRight: mediaMinSm ? '32px' : '6px',
-      }}>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'row',
-          alignItems: 'flex-end',
-          justifyContent: 'flex-end',
-        }}>
-          <TextField
-            InputLabelProps={{ style: { fontWeight: 'bold', color: 'orange' } }}
-            style={{
-              width: '100%',
-              padding: '0px',
-              margin: '0px',
-            }}
-            label="SHORT URL"
-            value={shortUrl}
-          />
-          {/* button to copy short url */}
-          <Button
-            style={{
-              position: 'absolute',
-              color: 'orange',
-              zIndex: '2',
-              cursor: 'pointer',
-            }}
-            onClick={() => {
-              navigator.clipboard.writeText(shortUrl);
-              dispatch(setIsSnackbarOpen({
-                isOpen: true, message: 'Copied', severity: 'success',
-              }));
-            }}>
-            Copy
-          </Button>
-        </div>
-        {/* displaying the time of creation */}
-        <p style={{ textAlign: 'center' }}>CREATED AT {new Date().toDateString()}</p>
-        <div style={{
-          display: 'flex',
-          flexDirection: 'column',
-          flex: '1',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
-          {/* form to save additional information of Url*/}
-          <form noValidate onSubmit={(e) => {
+    form = (
+      <Card style={{ marginTop: '10vh' }}>
+        <CardHeader title="Short url"
+          style={{
+            backgroundColor: 'black', color: 'white',
+            width: '100%',
+          }} />
+        {/* Form to save additional information of Url*/}
+        <form
+          noValidate
+          onSubmit={(e) => {
             e.preventDefault();
             handleUrlDetails();
-          }}>
+          }}
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            paddingTop: '4px',
+            paddingBottom: mediaMinSm ? '16px' : '6px',
+            paddingLeft: mediaMinSm ? '32px' : '6px',
+            paddingRight: mediaMinSm ? '32px' : '6px',
+            width: '100%',
+          }}
+        >
+          {/* Title */}
+          <TextField
+            title="Title"
+            label="Title"
+            value={urlTitle}
+            onChange={(e) => {
+              setUrlTitle(e.target.value);
+            }}
+            placeholder="Title"
+          />
+          {/* Long url */}
+          <TextField
+            title="Long url"
+            label="Long url"
+            placeholder="Long url"
+            value={queryLongUrl}
+            disabled
+          />
+          {/* Short url */}
+          <div style={{ display: 'flex' }}>
             <TextField
-              title="Title"
-              label="Title"
-              value={urlTitle}
-              onChange={(e) => {
-                setUrlTitle(e.target.value);
-              }}
-              multiline="true"
-              placeholder="Url title (optional)"
-              variant="outlined"
+              title="Short url"
+              label="Short url"
+              placeholder="Short url"
+              value={shortUrl}
+              style={{ flexGrow: 1 }}
             />
-            <TextField
-              title="Description"
-              label="Description"
-              value={urlDescription}
-              onChange={(e) => {
-                setUrlDescription(e.target.value);
-              }}
-              multiline="true"
-              placeholder="Url Description (optional)"
-              variant="outlined"
-            />
+            {/* Button to copy short url */}
+            <CardActions>
+              <Button
+                onClick={() => {
+                  navigator.clipboard.writeText(shortUrl);
+                  dispatch(setIsSnackbarOpen({
+                    isOpen: true, message: 'Copied', severity: 'success',
+                  }));
+                }}>
+              Copy
+              </Button>
+            </CardActions>
+          </div>
+          {/* Description */}
+          <TextField
+            title="Description"
+            label="Description"
+            value={urlDescription}
+            onChange={(e) => {
+              setUrlDescription(e.target.value);
+            }}
+            multiline={true}
+            placeholder="Url Description"
+          />
+          <div style={{ display: 'flex' }}>
+            {/* Time of creation */}
+            <span
+              style={{ flexGrow: 1, color: 'gray', margin: '8px', marginTop: '16px' }}>
+              <em>{new Date().toDateString()}</em>
+            </span>
+            {/* Submit update */}
             <Button
               type="submit"
               variant="contained"
               color="primary"
-              style={{
-                marginLeft: 'auto',
-              }}>
-            Add
+            >
+            Save
             </Button>
-          </form>
-        </div>
-      </div>);
+          </div>
+        </form>
+      </Card>
+    );
   }
 
   return (
     <div className={classes.root}>
-      <Card style={{
-        width: mediaMinSm ? '400px' : '100%',
-      }}>
-        <CardHeader
-          className={classes.cardHeader}
-          title="CREATE LINK"
-        />
-        {/* Only show progress bar when form is disabled */}
-        <LinearProgress
-          variant="query"
-          style={{
-            width: mediaMinSm ? '400px' : '100%',
-            visibility: isFormEnabled ? 'hidden' : 'visible',
-          }}
-        />
-        {content}
-      </Card>
+      {form}
+      {/* Only show progress bar when form is disabled */}
+      {!isFormEnabled &&
+        // Show skeletion loading
+        <div style={{ display: 'flex', flexDirection: 'column' }}>
+          <Skeleton
+            variant="rect"
+            style={{
+              height: '64px',
+            }} />
+          <Skeleton
+            variant="rect"
+            style={{
+              width: '50%',
+              height: '32px',
+              margin: '20px 16px 4px',
+            }} />
+          <Skeleton
+            variant="rect"
+            style={{
+              width: '95%',
+              height: '32px',
+              margin: '8px 16px 4px',
+            }} />
+          <Skeleton
+            variant="rect"
+            style={{
+              width: '78%',
+              height: '32px',
+              margin: '8px 4px 4px 16px',
+            }} />
+          <Skeleton
+            variant="rect"
+            style={{
+              width: '65%',
+              height: '32px',
+              margin: '8px 16px 4px',
+            }} />
+          <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Skeleton
+              variant="text"
+              style={{
+                width: '10%',
+                margin: '8px 4px 4px 16px',
+              }} />
+            <Skeleton
+              variant="rect"
+              style={{
+                width: '12%',
+                height: '32px',
+                margin: '8px 20px 4px',
+              }} />
+          </div>
+        </div>
+      }
     </div>
   );
 }
